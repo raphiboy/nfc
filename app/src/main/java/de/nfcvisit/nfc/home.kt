b5a004186dac90d5da231308f46ba8391fb8c6ce
Raphael <raphiboy@users.noreply.github.com>
2018-06-26 10:55:43 +0200

    Initial commit

---
+++ app/src/main/java/de/nfcvisit/nfc/home.kt
@@ -0,0 +1,12 @@
+package de.nfcvisit.nfc
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class home : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
